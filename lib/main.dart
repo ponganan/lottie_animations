b85c7944013bb10e4e4afb815fdf83e1065e2ed9
ponganan <joeyubu@gmail.com>
2022-08-18 15:01:00 +0700

    add Icon and Rename package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return _MyAppState();
@@ -14,10 +16,12 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            'Lottie Animaion',
+          backgroundColor: Colors.red,
+          title: const Text(
+            'JiJi App',
             style: TextStyle(
               fontSize: 30,
               fontWeight: FontWeight.w900,
@@ -27,14 +31,46 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Center(
           //margin: EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Lottie.network(
-                  'https://assets7.lottiefiles.com/packages/lf20_vasyynfq.json',
-                  //this line for start or stop animation
-                  // true or false
-                  animate: false),
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Lottie.network(
+                    'https://assets8.lottiefiles.com/packages/lf20_yoqwlhoa.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+                Lottie.network(
+                    'https://assets4.lottiefiles.com/packages/lf20_sw1zmjp3.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+                Lottie.network(
+                    'https://assets8.lottiefiles.com/private_files/lf30_dhkktwhk.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+                Lottie.network(
+                    'https://assets8.lottiefiles.com/packages/lf20_m2gdttai.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+                Lottie.network(
+                    'https://assets8.lottiefiles.com/packages/lf20_jjs6fnps.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+                Lottie.network(
+                    'https://assets8.lottiefiles.com/packages/lf20_udklubzk.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+                Lottie.network(
+                    'https://assets10.lottiefiles.com/packages/lf20_hevhmgvc.json',
+                    //this line for start or stop animation
+                    // true or false
+                    animate: true),
+              ],
+            ),
           ),
         ),
       ),
